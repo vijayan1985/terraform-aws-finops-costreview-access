550f415658c932a41159affe5d6a478b54387986
Alexander Wiechert <alexander.wiechert@deutschepost.de>
2025-05-08 14:08:52 +0200

    fix fmt

--- examples/iam-user/main.tf
+++ examples/iam-user/main.tf
@@ -1,7 +1,7 @@
 module "finops_access" {
-  source      = "../../"
-  mode        = "iam-user"
-  account_id  = "123456789012"
-  user_name   = "custom-finops-user"
-  role_name   = "custom-finops-role"
+  source     = "../../"
+  mode       = "iam-user"
+  account_id = "123456789012"
+  user_name  = "custom-finops-user"
+  role_name  = "custom-finops-role"
 }

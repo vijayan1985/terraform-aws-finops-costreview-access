acc21830cc77e7ad550df13b93b63b705db23470
Alexander Wiechert <alexander.wiechert@deutschepost.de>
2025-05-08 11:47:20 +0200

    Provider Mocking

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region                      = "eu-central-1"
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+}
